c0c310f16b826564a5659e42c01ad400194f1acc
snxrajput-kr <kr0471666@gmail.com>
2026-08-26 10:43:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -214,4 +214,6 @@ def rizer_endpoint():
     }), 401
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=False)
+    import os
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port, debug=False)
